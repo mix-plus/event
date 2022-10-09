8d516cc398b0c9167a7bfa20d118b5b2b7d43097
cexll <evanxian9@gmail.com>
2022-10-09 14:28:19 +0800

    update

--- src/EventDispatcherFactory.php
+++ src/EventDispatcherFactory.php
@@ -6,14 +6,12 @@ namespace MixPlus\Event;
 
 use Monolog\Logger;
 use Psr\Container\ContainerInterface;
-use Psr\EventDispatcher\ListenerProviderInterface;
 
 class EventDispatcherFactory
 {
     public function __invoke(ContainerInterface $container)
     {
-        $listeners = $container->get(ListenerProviderInterface::class);
         $log = new Logger("event");
-        return new EventDispatcher($listeners, $log);
+        return new EventDispatcher(new ListenerProvider, $log);
     }
 }
